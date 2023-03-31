d0ece3ae0047e8f2a7092b29c33c0be83576870f
Deepansh Saini <deepansh@kali>
2023-03-31 20:13:52 +0530

    git secret key updated

--- the_defenseloop_club/the_defenseloop_club/settings.py
+++ the_defenseloop_club/the_defenseloop_club/settings.py
@@ -20,8 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open( f'{BASE_DIR}/secretKey/secretKey.txt') as f:
-    SECRET_KEY = f.read().strip()
+# with open( f'{BASE_DIR}/secretKey/secretKey.txt') as f:
+#     SECRET_KEY = f.read().strip()
+
+SECRET_KEY = "django-insecure-cp3frd@nhg9+85f#!g$n7#nk+7upffv5ze-m5tvup3bw17u$bt"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
